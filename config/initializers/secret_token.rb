23b880c6ac421a364ffb085a99d16bec9c872005
Vishal Sharma <vishalsh@thoughtworks.com>
2013-11-27 19:27:38 +0530

    First commit to initialize app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BookSharingApp::Application.config.secret_key_base = 'ed4ee2db7ebc67103abe575708648d4b3a0685c324d8ab30fb56d9bf2a80ec32f9294c4cabc2365f61105cb3cf62d41ed3978173d15a6d744f7d46650be73b86'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BookSharingApp::Application.config.session_store :cookie_store, key: '_book-sharing-app_session'
