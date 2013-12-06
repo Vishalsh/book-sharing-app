745e5e734d059a036f9d6525ffaa59b945132823
tw <tw@tw2.(none)>
2013-12-06 12:05:45 +0530

    trying deploy

--- startserver.sh
+++ startserver.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 #ssh user@10.10.5.111 /home/user/BookSharing/current
-#PID = $(ps aux | grep rails | grep -v grep | awk '{ print $2 }')
 BUILD_ID=dontKillMe
 sudo kill -9 $(ps aux | grep rails | grep -v grep | awk '{ print $2 }')
 rails s &
